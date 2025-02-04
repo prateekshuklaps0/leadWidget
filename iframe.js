c7414f8abb3284123316bfd63f371b9e892fef10
Prateek Shukla <112534172+prateekshuklaps0@users.noreply.github.com>
2025-02-04 15:30:30 +0530

    Update iframe.js

--- iframe.js
+++ iframe.js
@@ -1,1 +1,1 @@
-class iFrameWidget{constructor(t){this.widgetId=t;const e=new URLSearchParams(window.location.search),o=document.getElementById(t);let c="http://localhost:7001/widget/";c+=t,e.toString()&&(c+="?"+e.toString()),o.src=c}}
+class LoadWidget{constructor(t){this.widgetId=t;const o=new URLSearchParams(window.location.search),e=document.getElementById(t);let c="http://localhost:7001/widget/";c+=t,o.toString()&&(c+="?"+o.toString()),e.src=c}}
