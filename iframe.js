7d46717cffbae5356d88de7a1e93eff569576309
Prateek Shukla <112534172+prateekshuklaps0@users.noreply.github.com>
2025-02-11 16:52:12 +0530

    Update iframe.js

--- iframe.js
+++ iframe.js
@@ -1,1 +1,14 @@
-class LoadWidget{constructor(t){this.widgetId=t;const o=new URLSearchParams(window.location.search),e=document.getElementById(t);if(!e)return console.warn(`Couldn't load iframe with id : '${t}'`);let n=`https://lead.mastersunion.org/widget/${t}`;o.toString()&&(n+="?"+o.toString()),e.src=n}}
+class LoadWidget {
+    constructor(widgetId) {
+        const urlParams = new URLSearchParams(window.location.search);
+        const iframe = document.getElementById(widgetId);
+        if (!iframe) return console.warn(`Couldn't load iframe with id : '${widgetId}'`);
+        iframe.setAttribute("frameBorder", "0")
+        // let iFrameSrc = `https://lead.mastersunion.org/widget/${widgetId}`;
+        let iFrameSrc = `http://localhost:7001/${widgetId}`;
+        if (urlParams.toString()) {
+            iFrameSrc += "?" + urlParams.toString();
+        }
+        iframe.src = iFrameSrc;
+    }
+}
