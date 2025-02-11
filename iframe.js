34c0df127b60b3abbe866b46a3e8e9c5e12678e7
Prateek Shukla <112534172+prateekshuklaps0@users.noreply.github.com>
2025-02-11 17:40:05 +0530

    Update iframe.js

--- iframe.js
+++ iframe.js
@@ -1,14 +1,21 @@
 class LoadWidget {
     constructor(widgetId) {
-        const urlParams = new URLSearchParams(window.location.search);
-        const iframe = document.getElementById(widgetId);
-        if (!iframe) return console.warn(`Couldn't load iframe with id : '${widgetId}'`);
-        iframe.setAttribute("frameBorder", "0")
-        // let iFrameSrc = `https://lead.mastersunion.org/widget/${widgetId}`;
-        let iFrameSrc = `http://localhost:7001/widget/${widgetId}`;
-        if (urlParams.toString()) {
-            iFrameSrc += "?" + urlParams.toString();
-        }
-        iframe.src = iFrameSrc;
+        const checkAndLoad = () => {
+            const iframe = document.getElementById(widgetId);
+            if (!iframe) {
+                setTimeout(checkAndLoad, 100);
+                return;
+            }
+            iframe.setAttribute("frameBorder", "0");
+            // let iFrameSrc = `https://lead.mastersunion.org/widget/${widgetId}`;
+            let iFrameSrc = `http://localhost:7001/widget/${widgetId}`;
+            const urlParams = new URLSearchParams(window.location.search);
+            if (urlParams.toString()) {
+                iFrameSrc += "?" + urlParams.toString();
+            }
+            iframe.src = iFrameSrc;
+        };
+
+        document.addEventListener("DOMContentLoaded", checkAndLoad);
     }
 }
